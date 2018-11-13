a3720d19b28cd115af7e6675785bb00752f86236
TomBud1 <tommmbud@gmail.com>
2018-11-13 21:14:36 +0100

    add methods to HibernateUtility, refactoring

--- src/main/java/example/HibernateExample.java
+++ src/main/java/example/HibernateExample.java
@@ -1,5 +1,7 @@
 package example;
 
+import java.util.List;
+
 import example.entity.Customer;
 
 public class HibernateExample {
@@ -9,9 +11,17 @@ public class HibernateExample {
 		HibernateUtility hibernateUtility= new HibernateUtility();
 		
 		try {
-			Customer tempCustomer = new Customer("Alfred", "Gromee", "alfred@test.com");
-			hibernateUtility.createNew(tempCustomer);
-			hibernateUtility.getById(tempCustomer);
+			//Customer tempCustomer = new Customer("Alfred", "Gromee", "alfred@test.com");
+			//hibernateUtility.createNew(tempCustomer);
+			//hibernateUtility.getById(8);
+			//List<Customer> theCustomers = hibernateUtility.getAll();
+			
+			List<Customer> theCustomers = hibernateUtility.getWhere("email", "jerryt@test.com");
+			for(Customer tempCustomer : theCustomers) {
+				System.out.println(tempCustomer);
+			}
+			System.out.println("Done!");
+			
 		}
 		finally {
 			hibernateUtility.factoryClose();
--- src/main/java/example/HibernateUtility.java
+++ src/main/java/example/HibernateUtility.java
@@ -2,6 +2,9 @@ package example;
 
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
+
+import java.util.List;
+
 import org.hibernate.Session;
 import example.entity.Customer;
 import org.hibernate.cfg.Configuration;
@@ -18,10 +21,58 @@ public class HibernateUtility {
 	//create session
 	private Session session = factory.getCurrentSession();
 	
-	// use the session object to save Java object
+	public List<Customer> getAll(){
+		
+		session = factory.getCurrentSession();
+		
+		//start a transaction
+		session.beginTransaction();
+		
+		//query customers
+		List<Customer> customers = session.createQuery("from Customer").getResultList();
+		
+		return customers; 
+	}
+	
+	public Customer getById(int id) {
+		
+		session = factory.getCurrentSession();
+		
+		//retrieve student based on the id:primary key
+		System.out.println("\nGetting student with id: " + id);
+		
+		session.beginTransaction();
+		
+		Customer tempCustomer = session.get(Customer.class, id);
+		
+		session.getTransaction().commit();
+		
+		System.out.println("Get complete:");
+		System.out.println(tempCustomer.toString());
+		System.out.println("Done!");
+				
+		return tempCustomer;
+	}
+	
+	public List<Customer> getWhere(String column, String key) {
+		
+		session = factory.getCurrentSession();
+		
+		session.beginTransaction();
+		
+		String query = new String();
+		query = "from Customer c where c." + column + "='" + key + "'";
+		
+		List<Customer> customers = session.createQuery(query).getResultList();
+		
+		return customers;
+		
+	}
+	
 	
 	public void createNew(Customer tempCustomer) {
 		
+		// use the session object to save Java object
 		session = factory.getCurrentSession();
 	
 		// create a customer object
@@ -40,27 +91,13 @@ public class HibernateUtility {
 		System.out.println("Done!");				
 	}
 	
-	public Customer getById(Customer tempCustomer) {
-	
-		session = factory.getCurrentSession();
+	public void deleteById(int id) {
 		
-		//retrieve student based on the id:primary key
-		System.out.println("\nGetting student with id: " + tempCustomer.getId());
-		
-		session.beginTransaction();
-		
-		Customer myCustomer = session.get(Customer.class, tempCustomer.getId());
-		
-		session.getTransaction().commit();
-		
-		System.out.println("Get complete:");
-		System.out.println(myCustomer.toString());
-		System.out.println("Done!");
-				
-		return myCustomer;
 	}
 
 	public void factoryClose() {
+		session.flush();
+		session.close();
 		factory.close();
 	}
 
